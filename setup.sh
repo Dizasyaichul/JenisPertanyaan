54c93104c62de9f44275ee57c50d4931e1decfd6
Dizasyaichul <157806494+Dizasyaichul@users.noreply.github.com>
2025-02-11 15:12:53 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # Create nltk_data directory if it doesn't exist
 mkdir -p nltk_data
+
 # Download NLTK data to nltk_data directory
 python3 -m nltk.downloader -d ./nltk_data punkt
 python3 -m nltk.downloader -d ./nltk_data wordnet
+python3 -m nltk.downloader -d ./nltk_data averaged_perceptron_tagger
