2489c77e104e2528da0fafaeea15e1d355eeae97
Dizasyaichul <157806494+Dizasyaichul@users.noreply.github.com>
2025-02-11 14:46:53 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-
-# Buat direktori nltk_data jika belum ada
+# Create nltk_data directory if it doesn't exist
 mkdir -p nltk_data
-
-# Unduh data NLTK ke direktori nltk_data
+# Download NLTK data to nltk_data directory
 python3 -m nltk.downloader -d ./nltk_data punkt
+python3 -m nltk.downloader -d ./nltk_data wordnet
